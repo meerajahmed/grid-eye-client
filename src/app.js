3b89ae09ef5c72409319d89e8f5aac322ee09ff5
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2019-07-26 04:53:23 +0530

    Option one

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import GridEye from "./GridEye";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GridEye />
+      </header>
+    </div>
+  );
+}
+
+export default App;
