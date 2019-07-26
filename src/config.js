de06531befb01558e3be00a50e8793ccb38d763d
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2019-07-26 17:18:51 +0530

    Deploy on heroku

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ export default {
     WRAPPER_ID: 'id-notify',
     MIN_THRESHOLD: 1,
     MAX_THRESHOLD: 3,
-    SOCKET_URI:'http://localhost:3000'
+    SOCKET_URI:'https://cosmos-grid-eye.herokuapp.com'
 }
